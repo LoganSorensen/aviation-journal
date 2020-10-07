c3ed07a1eb8898d98a96e3d03671672630cf1485
Logan Sorensen <loganmsorensen@gmail.com>
2020-10-06 17:05:07 -0700

    added basic homepage structure

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <div className="image-cont">
-                
-            </div>
+  return (
+    <div>
+      <div className="image-window">
+          <h1>Logo</h1>
+          <div className="cta">
+              <span>Check out some stuff!</span>
+              <a href='#featured'><i className="fas fa-chevron-down"></i></a>
+          </div>
+      </div>
+      <div className="transition"></div>
+      <div className="lower-content" id='featured'>
+        <div className="featured-article"></div>
+        <div className="featured-articles">
+            <div className="article"></div>
+            <div className="article"></div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
